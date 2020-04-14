4b6a83581527c5c9f5da09925455dfa9df17a7cb
oouuyab <01073785645@naver.com>
2020-04-14 18:03:41 +0900

    완료

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import Nav from './Nav';
-import VideoPlayer from './VideoPlayer';
-import VideoList from './VideoList';
-import { fakeData } from './__test__/fakeData';
+import React from "react";
+import Nav from "./Nav";
+import VideoPlayer from "./VideoPlayer";
+import VideoList from "./VideoList";
+import { fakeData } from "./__test__/fakeData";
 
-const App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer />
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={fakeData} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: fakeData,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Nav />{" "}
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.data[0]} />{" "}
+        </div>{" "}
+        <div className="col-md-5">
+          <VideoList videos={this.state.data} />{" "}
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   const [data, setData] = useState(fakeData);
+//   return (
+//     <div>
+//       <Nav />
+//       <div className="col-md-7">
+//         <VideoPlayer video={data[0]} />
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={data} />
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
