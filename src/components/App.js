58f1dec174f0d1cbafe98cdefe9df595f48631ad
oouuyab <01073785645@naver.com>
2020-04-16 15:59:26 +0900

    currentVideo

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,30 @@ import Nav from "./Nav";
 import VideoPlayer from "./VideoPlayer";
 import VideoList from "./VideoList";
 import { fakeData } from "./__test__/fakeData";
-import searchYoutube from "./../searchYoutube";
-
+import { searchYouTube } from "./../searchYouTube";
+import { YOUTUBE_API_KEY } from "../../config/youtube";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentVideo: "",
       videoList: fakeData,
     };
     this.serchClick = this.serchClick.bind(this);
   }
+
+  componentDidMount() {
+    this.serchClick("codestates", 5, YOUTUBE_API_KEY);
+  }
+
   serchClick(query, max, key) {
-    searchYoutube(query, max, key, (data) => {
-      this.setState({ videoList: data });
+    console.log(query, max, key);
+    console.log(searchYouTube);
+    searchYouTube({ query, max, key }, (data) => {
+      this.setState({
+        currentVideo: data[0],
+        videoList: data,
+      });
     });
   }
 
@@ -24,7 +35,7 @@ class App extends React.Component {
       <div>
         <Nav searchVideo={this.serchClick} />{" "}
         <div className="col-md-7">
-          <VideoPlayer video={this.state.videoList[0]} />{" "}
+          <VideoPlayer video={this.state.currentVideo} />{" "}
         </div>{" "}
         <div className="col-md-5">
           <VideoList videos={this.state.videoList} />{" "}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { YOUTUBE_API_KEY } from "../../config/youtube";
-const Search = (props) => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button
-      onClick={() => props.searchVideo("qweqwe", 5, YOUTUBE_API_KEY)}
-      className="btn hidden-sm-down"
-    >
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div>
-);
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input
+          onChange={this.handleChange}
+          className="form-control"
+          type="text"
+        />
+        <button
+          onClick={() => {
+            this.props.searchVideo(this.state.value, 5, YOUTUBE_API_KEY);
+          }}
+          className="btn hidden-sm-down"
+        >
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Search;
